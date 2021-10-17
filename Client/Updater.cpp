a5b95063983dbb19b912126ebfd33a260e316f1f
Daniel Reed <x@sys32.dev>
2021-10-17 02:51:06 -0400

    catch serving parse errors

--- Client/Updater.cpp
+++ Client/Updater.cpp
@@ -1,11 +1,19 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "./httplib.h"
 #include "./Updater.h"
+#include "./Log.h"
 
 JSON Updater::GetServing() {
 	httplib::Client cli(host);
 	auto res = cli.Get(path.c_str());
-	return JSON::parse(res->body);
+	
+	try {
+		return JSON::parse(res->body);
+	}
+	catch (JSON::parse_error err) {
+		clog::error << "Error parsing serving: " << err.what() << clog::endl;
+		return JSON::object();
+	}
 }
 
 bool Updater::UpdatesAvailable(std::string& url) {
