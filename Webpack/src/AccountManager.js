01a27121384750be97b2bf0c35fb550352686986
Daniel Reed <x@sys32.dev>
2021-10-29 22:53:27 -0400

    one opts for all

--- Webpack/src/AccountManager.js
+++ Webpack/src/AccountManager.js
@@ -9,25 +9,7 @@ class Menu {
 	window = new Window(this);
 	save_config(){}
 	async attach(){
-		var sin = await utils.wait_for(() => document.querySelector('#signedInHeaderBar')),
-			sout = await utils.wait_for(() => document.querySelector('#signedOutHeaderBar'));
-		
-		tick(utils.add_ele('div', sin, {
-			className: 'button buttonG lgn',
-			style: {
-				width: '300px',
-				'margin-right': 0,
-				'padding-top': '5px',
-				'margin-left': '5px',
-				'padding-bottom': '13px',
-			},
-			innerHTML: `Accounts <span class="material-icons" style="vertical-align:middle;color: #fff;font-size:36px;margin-top:-8px;">switch_account</span>`,
-			events: {
-				click: () => this.window.show(),
-			},
-		}));
-		
-		tick(utils.add_ele('div', sout, {
+		var opts = {
 			className: 'button buttonG lgn',
 			style: {
 				width: '200px',
@@ -40,7 +22,14 @@ class Menu {
 			events: {
 				click: () => this.window.show(),
 			},
-		}));
+		};
+		
+		var sin = await utils.wait_for(() => document.querySelector('#signedInHeaderBar')),
+			sout = await utils.wait_for(() => document.querySelector('#signedOutHeaderBar'));
+			
+		
+		tick(utils.add_ele('div', sin, opts));
+		tick(utils.add_ele('div', sout, opts));
 		
 		this.window.attach(await utils.wait_for(() => document.querySelector('#uiBase')));
 	}
