3de0b630f9c1357d8ee18c4220469e762619d563
v5p <viii.tap@proton.me>
2022-10-02 16:10:20 -0400

    format

--- src/TraverseCopy.cpp
+++ src/TraverseCopy.cpp
@@ -3,27 +3,33 @@
 
 using JSON = nlohmann::json;
 
-JSON TraverseCopy(JSON value, JSON match, bool allow_new_props, bool* changed) {
-	if (value.type() != match.type()) {
-		if (changed) *changed = true;
+JSON TraverseCopy(JSON value, JSON match, bool allow_new_props, bool *changed)
+{
+	if (value.type() != match.type())
+	{
+		if (changed)
+			*changed = true;
 		return match;
 	}
 
-	if (value.is_object()) {
+	if (value.is_object())
+	{
 		JSON result = match;
 
-		for (auto [skey, svalue] : value.items()) {
-			if (match.contains(skey)) result[skey] = TraverseCopy(svalue, match[skey], allow_new_props, changed);
-			else if (allow_new_props && !result.contains(skey)) {
+		for (auto [skey, svalue] : value.items())
+		{
+			if (match.contains(skey))
+				result[skey] = TraverseCopy(svalue, match[skey], allow_new_props, changed);
+			else if (allow_new_props && !result.contains(skey))
 				result[skey] = svalue;
-				// if (changed) *changed = true;
-			}
-			else if (changed) *changed = true;
+			else if (changed)
+				*changed = true;
 		}
 
 		return result;
 	}
-	else {
+	else
+	{
 		return value;
 	}
 }
