1f1ef40a9f90f7a44fc764921f02c1eed3c7ef7c
v5p <viii.tap@proton.me>
2022-10-04 16:39:06 -0400

    remove use strict from scripts

--- scripts/Adblock.js
+++ scripts/Adblock.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const metadata = {
 	"name": "Adblock",
 	"author": "Chief Software",
--- scripts/Sky Color.js
+++ scripts/Sky Color.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const metadata = {
 	"name": "Sky Color",
 	"author": "Chief Software",
