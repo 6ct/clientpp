46eefa08ffca0c20b6374a389f6193e1fa65375f
v5p <viii.tap@proton.me>
2022-10-04 23:39:34 -0400

    src

--- frontend/src/menu/settHolderProxy.ts
+++ frontend/src/menu/settHolderProxy.ts
@@ -1,22 +1,36 @@
 /**
  * Unique HTML string that acts as a fragment.
+ * Elements of the fragment will be recalled before being unloaded. Elements are always reused and never need to be recreated.
  */
-export default class HTMLProxy extends DocumentFragment {
-  #id = "a-" + Math.random().toString().slice(2);
-  constructor() {
-    super();
+export default function createSettHolderProxy() {
+  const id = "a-" + Math.random().toString().slice(2);
 
-    const replaceWith = (proxy: HTMLProxyElement) => proxy.replaceWith(this);
+  const fragment = new DocumentFragment();
 
-    class HTMLProxyElement extends HTMLElement {
-      connectedCallback() {
-        replaceWith(this);
-      }
+  class HTMLProxyElement extends HTMLElement {
+    connectedCallback() {
+      const settHolder = this.parentElement! as HTMLDivElement;
+
+      Reflect.defineProperty(settHolder, "innerHTML", {
+        configurable: true,
+        set: (html) => {
+          for (const child of settHolder.childNodes) fragment.append(child);
+
+          // remove descriptor
+          Reflect.deleteProperty(settHolder, "innerHTML");
+
+          settHolder.innerHTML = html;
+        },
+      });
+
+      this.replaceWith(fragment);
     }
+  }
 
-    customElements.define(this.#id, HTMLProxyElement);
-  }
-  get() {
-    return `<${this.#id} />`;
-  }
+  customElements.define(id, HTMLProxyElement);
+
+  return {
+    fragment,
+    html: `<${id} />`,
+  };
 }
