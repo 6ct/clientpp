7046b5909b5f9d37217958fbff28abe708211521
v5p <viii.tap@proton.me>
2022-10-08 21:28:08 -0400

    rework adblocking to target FRVR

--- scripts/adblock.chief.js
+++ scripts/adblock.chief.js
@@ -11,37 +11,60 @@ function main() {
   // set at page load
   const enabled = localStorage.getItem("adblock enabled") === "on";
 
-  const adblock =
-    /^https:\/\/(?:krunker.io\/libs\/frvr-|cookie-cdn\.cookiepro\.com\/|api.adinplay.com\/|www\.googletagmanager\.com\/|pagead2\.googlesyndication\.com\/pagead\/|a\.pub\.network\/|unpkg\.com\/web3@latest\/dist\/web3\.min\.js)/;
+  if (enabled) {
+    const style = document.createElement("style");
+    style.textContent = "#adCon, *[id*='aHider'] { display: none !IMPORTANT; }";
 
-  function filterURL(url) {
-    if (!enabled) return false;
+    document.addEventListener("DOMContentLoaded", () => {
+      document.documentElement.append(style);
+    });
 
-    return adblock.test(url.toString());
+    const shimFRVR = Object.freeze({
+      init: () => {},
+      lifecycle: Object.freeze({}),
+      tracker: Object.freeze({
+        addExtraFieldFunction: () => {},
+        logEvent: () => {},
+      }),
+      bootstrapper: Object.freeze({
+        init: () => Promise.resolve(),
+        setProgress: () => {},
+        complete: () => {},
+      }),
+    });
+
+    // :((((
+    // we break the AD loader in head by making this non-configurable :(
+    Object.defineProperty(window, "FRVR", {
+      get: () => shimFRVR,
+      set: (value) => {
+        // krunker sets FRVR = window.FRVR || {}
+        // we can detect this expression by checking if the value is equal to window.FRVR
+        if (value === shimFRVR) {
+          console.error("Caught ADs.");
+          throw new Error("Adblock");
+        }
+      },
+    });
+
+    // now we have to configure the AD networks ourselves...
+    window.canShowAds = false;
+    window.useFRANads = false;
+    window.useFreestarAds = false;
+    window.useAdinplayAds = false;
+
+    // and the app restrictions...
+    window.canShowExternalLinks = true;
+    window.canShowPaypal = false;
+    window.canShowSocialHub = true;
+    window.canShowMods = true;
+    window.canConnectExternalAccounts = true;
+    window.canShowMarketplace = true;
+    window.canShowNFTs = true;
+    window.canShowTwitch = true;
+    window.canShowKrunkerEngine = true;
+    window.isFrvrDotCom = false;
   }
-
-  new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes)
-        if (node instanceof HTMLScriptElement && filterURL(node.src))
-          node.remove();
-    }
-  }).observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  });
-
-  window.FRVR = {
-    init: () => {},
-    lifecycle: {},
-  };
-
-  const style = document.createElement("style");
-  style.textContent = "#adCon, *[id*='aHider'] { display: none !IMPORTANT; }";
-
-  document.addEventListener("DOMContentLoaded", () => {
-    document.documentElement.append(style);
-  });
 }
 
 function Settings() {
