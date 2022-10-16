27d2e2bbd656be8a59b660d130edd9633aa4277e
v5p <viii.tap@proton.me>
2022-10-16 15:46:05 -0400

    fix sourcemaps, add terser plugin

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import MagicString from "magic-string";
-import sourcemaps from "rollup-plugin-sourcemaps";
+import { terser } from "rollup-plugin-terser";
 
 /**
  * @return {import("rollup").Plugin}
@@ -19,7 +19,7 @@ const tryCatch = () => {
   };
 };
 
-// const isDevelopment = process.env.NODE_ENV === "development";
+const isDevelopment = process.env.NODE_ENV === "development";
 
 /**
  * @type {import("rollup").RollupOptions[]}
@@ -29,7 +29,11 @@ const configs = [
     input: "./src/game.ts",
     output: {
       sourcemap: "inline",
-      sourcemapBaseUrl: "chief://the/frontend/",
+      sourcemapPathTransform: (relativeSourcePath) =>
+        new URL(
+          "./frontend/" + relativeSourcePath,
+          "rollup://chief/frontend/"
+        ).toString(),
       dir: "./dist/",
       format: "iife",
     },
@@ -37,25 +41,34 @@ const configs = [
       commonjs(),
       nodeResolve(),
       typescript({
+        sourceMap: true,
         inlineSources: true,
       }),
-      sourcemaps(),
       tryCatch(),
+      isDevelopment && terser(),
     ],
   },
   {
     input: "./src/generic.ts",
     output: {
       sourcemap: "inline",
+      sourcemapPathTransform: (relativeSourcePath) =>
+        new URL(
+          "./frontend/" + relativeSourcePath,
+          "rollup://chief/frontend/"
+        ).toString(),
       dir: "./dist/",
       format: "iife",
     },
     plugins: [
       commonjs(),
       nodeResolve(),
-      typescript(),
-      sourcemaps(),
+      typescript({
+        sourceMap: true,
+        inlineSources: true,
+      }),
       tryCatch(),
+      isDevelopment && terser(),
     ],
   },
 ];
