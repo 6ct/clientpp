3afc58f8887c23f57836d9873c7ea6283d20451a
v5p <viii.tap@proton.me>
2022-11-12 13:19:15 -0500

    polish idkr scripts

--- scripts/example.idkr.js
+++ scripts/example.idkr.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class Userscript {
   constructor() {
     this.meta = {
--- scripts/old example.idkr.js
+++ scripts/old example.idkr.js
@@ -1,46 +1,27 @@
-module.exports = {
+"use strict";
+
+const userscript = {
   // Name of the script
   name: "Selectable Chat",
-
   // Name of the author
   author: "Mixaz",
-
   // Version of the script
   version: "1.0.0",
-
   // Allowed values: "all" | "game" | "social" | "viewer" | "editor"
   locations: ["game"],
-
-  settings: {
-    disableFrameRateLimit: {
-      name: "Disable Frame Rate Limit",
-      id: "disableFrameRateLimit",
-      cat: "Performance",
-      type: "checkbox",
-      val: true,
-      needsRestart: true,
-      html() {
-        return clientUtils.genCSettingsHTML(this);
-      },
-    },
-  },
-
   // Callback function to run
   // The first argument is an instance of electron-store to allow easier config operations (not used in this example)
-  run: (config) => {
-    console.log(
-      "disableFrameRateLimit:",
-      config.get("disableFrameRateLimit", true)
-    );
+  // run: (config) => {...}
+  run: () => {
+    const newCSS = document.createElement("style");
 
-    const newCSS = Object.assign(document.createElement("style"), {
-      id: "selectableChat",
-      innerHTML: `#chatList * {
-				user-select: text;
-			}`,
-    });
+    newCSS.id = "selectableChat";
+    newCSS.innerHTML = "#chatList * { user-select: text; }";
+
     document.addEventListener("DOMContentLoaded", () =>
       document.head.appendChild(newCSS)
     );
   },
 };
+
+module.exports = userscript;
