c71d00ece23dedd4e361f162aa1684a3aa951fb9
v5p <viii.tap@proton.me>
2023-01-02 12:16:20 -0500

    upgrade dependencies
    we have to downgrade @rollup/plugin-terser for sourcemap support
    some high severity vulnerabilities are present

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import MagicString from "magic-string";
-import { terser } from "rollup-plugin-terser";
 
 /**
  * @return {import("rollup").Plugin}
